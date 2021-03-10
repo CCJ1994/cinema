2e94c80886bed4f3d12e12f9dee1f4942f93d281
ccj1994 <a0changj2@gmail.com>
2021-03-10 22:07:42 +0800

    index layout

---
+++ js/myjs.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+  function bgmenu(){//判斷如果scroll是否在section#meslider內來決定是否提供bg-dark
+    const 
+    nowat=$(document).scrollTop(),
+    height=$("#bannerSlider").innerHeight(),
+    offset=$("#navMenu").innerHeight(),
+    screenW=$(document).innerWidth();
+
+    if(screenW>=992){
+
+      if(nowat<height-offset){
+        $("#navMenu").removeClass("bg-dark");
+      }else{
+        $("#navMenu").addClass("bg-dark");
+      }
+    }else{
+      $("#navMenu").addClass("bg-dark");
+    }
+
+  }
+
+  $(document).scroll(function(){//對document進行滾動
+    bgmenu();
+  });
+  $(window).resize(bgmenu); //對window重新調整尺寸時
+  bgmenu();
+})
